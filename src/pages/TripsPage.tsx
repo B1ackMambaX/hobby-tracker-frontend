d6c7a98b3f02c7d26ea85376bfb704dd2d8244fb
B1ackMambaX <b1ackmambax@yandex.ru>
2025-06-15 19:24:37 +0500

    Feature: Added empty main page

--- src/pages/TripsPage.tsx
+++ src/pages/TripsPage.tsx
@@ -3,12 +3,12 @@ import TripsList from "@/components/tripsList/TripsList.tsx";
 import PageLayout from "@/components/ui/pageLayout/PageLayout.tsx";
 import {ActivePage} from "@/components/navBar/navBar.props.ts";
 
-const MainPage = () => {
+const TripsPage = () => {
     return <Layout>
-        <PageLayout activePage={ActivePage.HOME} headingText="Мои путешествия">
+        <PageLayout activePage={ActivePage.TRIPS} headingText="Мои путешествия">
             <TripsList/>
         </PageLayout>
     </Layout>
 }
 
-export default MainPage
+export default TripsPage
---
+++ src/pages/homePage/HomePage.tsx
@@ -0,0 +1,19 @@
+import {useSelector} from "react-redux";
+import {RootState} from "@/store";
+import Layout from "@/components/ui/layout/Layout.tsx";
+import PageLayout from "@/components/ui/pageLayout/PageLayout.tsx";
+import {ActivePage} from "@/components/navBar/navBar.props.ts";
+
+const HomePage = () => {
+    const {user} = useSelector((state: RootState) => state.authReducer);
+
+    return <Layout>
+        <PageLayout
+            activePage={ActivePage.HOME}
+            headingText={user ? `Привет, ${user.name}` : 'TravelTracker'}
+        >
+        </PageLayout>
+    </Layout>
+}
+
+export default HomePage;
